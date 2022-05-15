c106eced6692c19931c4323f4e89223b765ba92d
td236 <dadela.tomasz@gmail.com>
2022-05-15 22:26:30 +0200

    tests: remove irrelevant tests

--- task2/tests/calendar.py
+++ task2/tests/calendar.py
@@ -134,17 +134,6 @@ class TestCalendarMethods(unittest.TestCase):
         self.assertEqual(start, datetime(2022, 6, 2, 0, 0))
         self.assertEqual(end, MAX_DATE)
 
-    def test_isupper(self):
-        self.assertTrue('FOO'.isupper())
-        self.assertFalse('Foo'.isupper())
-
-    def test_split(self):
-        s = 'hello world'
-        self.assertEqual(s.split(), ['hello', 'world'])
-        # check that s.split fails when the separator is not a string
-        with self.assertRaises(TypeError):
-            s.split(2)
-
 
 if __name__ == '__main__':
     unittest.main()
